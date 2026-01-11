2d4cb208f92105ed18ffc1568dc83c0af981b89e
Nishuhhh <priyanshsharma2810@gmail.com>
2026-01-11 22:52:26 +0530

    production url changes for socket

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
+
 
 // Create socket instance (don't connect yet)
 export const socket = io(SOCKET_URL, {
